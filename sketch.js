40c389105a7e6f23f2983e9aa26f0a9faecd095f
MistaFish31 <zhang.ma@husky.neu.edu>
2016-01-24 22:41:38 -0500

    table
    table of ellipses with nested for loops
    (I forgot to fork when I started, so I copied and pasted the work I did
    before into this new repository)

--- sketch.js
+++ sketch.js
@@ -17,10 +17,24 @@
   }
 */
 
+function table(w,h){
+  var x=50
+  var y=50
+  
+  for(var i=0; i<w; i=i+2){
+    for (var j=0; j<h; j=j+2){
+      ellipse(x,y,i,j)
+      x=x+32
+    }
+    y=y+32
+    x=50
+  }
+}
+  
 function setup() {
-
+  createCanvas (1400,1400)
 }
 
 function draw() {
-
+  table(16,16)
 }
