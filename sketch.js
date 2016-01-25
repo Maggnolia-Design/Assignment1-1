0da31fe8f86437b05ecd0271ed2c9a9866cfef2d
MistaFish31 <zhang.ma@husky.neu.edu>
2016-01-24 22:47:04 -0500

    colors
    light blue stroke and fill

--- sketch.js
+++ sketch.js
@@ -1,28 +1,11 @@
-/*
-  // A loop is structued like this:
-  keyword (condition) {
-    stuff that happens
-  }
-
-  // while loop
-  // (condition)
-  while (keep going as long as this evaluates to true) {
-    // stuff happens
-  }
-
-  // for loop:
-  // (variable assignment; condition to run while true; change to iterator)
-  for(var i = 0; i < 20; i = i+ 1) {
-    // stuff happens
-  }
-*/
-
 function table(w,h){
   var x=50
   var y=50
   
   for(var i=0; i<w; i=i+2){
     for (var j=0; j<h; j=j+2){
+      fill(100,100,250)
+      stroke(100,100,250)
       ellipse(x,y,i,j)
       x=x+32
     }
